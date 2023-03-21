b78978d6a92c06e0e5a128c945ade514c5d99186
Aitooor <aitorarias19@gmail.com>
2023-03-22 00:42:07 +0100

    fix: added correctly commands

--- src/main/java/online/starsmc/spawn/module/CommandModule.java
+++ src/main/java/online/starsmc/spawn/module/CommandModule.java
@@ -2,12 +2,18 @@ package online.starsmc.spawn.module;
 
 import me.fixeddev.commandflow.annotated.CommandClass;
 import online.starsmc.spawn.command.MainCommand;
+import online.starsmc.spawn.command.SetSpawnCommand;
+import online.starsmc.spawn.command.SpawnCommand;
 import team.unnamed.inject.AbstractModule;
 
 public class CommandModule extends AbstractModule {
 
         @Override
         protected void configure() {
-            multibind(CommandClass.class).asSet().to(MainCommand.class);
+            multibind(CommandClass.class)
+                    .asSet()
+                    .to(MainCommand.class)
+                    .to(SetSpawnCommand.class)
+                    .to(SpawnCommand.class);
         }
 }
