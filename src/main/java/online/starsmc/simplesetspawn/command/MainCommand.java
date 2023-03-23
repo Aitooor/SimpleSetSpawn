5196704f139a31c8fe6f73b237bf4ff049a6c464
Aitooor <aitorarias19@gmail.com>
2023-03-23 04:35:33 +0100

    fix: added spawns.yml to reload command

--- src/main/java/online/starsmc/simplesetspawn/command/MainCommand.java
+++ src/main/java/online/starsmc/simplesetspawn/command/MainCommand.java
@@ -4,16 +4,22 @@ import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import online.starsmc.simplesetspawn.Main;
+import online.starsmc.simplesetspawn.utils.BukkitConfiguration;
 import online.starsmc.simplesetspawn.utils.ChatUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import team.unnamed.inject.InjectAll;
+
+import javax.inject.Named;
 
 @InjectAll
 @Command(names = {"SimpleSetSpawn", "sss", "ssetspawn", "ssspawn"}, permission = "simplesetspawn.admin")
 public class MainCommand implements CommandClass {
 
     private Main plugin;
+
+    @Named("spawns")
+    private BukkitConfiguration spawnsConfig;
 
     @Command(names = {"reload", "rl"}, permission = "simplesetspawn.reload")
     public void reloadCommand(@Sender CommandSender sender){
@@ -24,6 +30,7 @@ public class MainCommand implements CommandClass {
         }
         Player player = (Player) sender;
         plugin.reloadConfig();
+        spawnsConfig.reload();
         ChatUtil.sendMsgPlayerPrefix(player, "&cReloaded correctly");
     }
 }
