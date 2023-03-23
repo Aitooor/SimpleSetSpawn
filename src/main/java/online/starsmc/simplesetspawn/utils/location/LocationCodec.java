9377e668f12a9459384b8c06ca226ebb541bb4a5
Aitooor <aitorarias19@gmail.com>
2023-03-23 04:35:06 +0100

    fix: LocationCodec change parse to float correctly

--- src/main/java/online/starsmc/simplesetspawn/utils/location/LocationCodec.java
+++ src/main/java/online/starsmc/simplesetspawn/utils/location/LocationCodec.java
@@ -21,8 +21,8 @@ public class LocationCodec {
             double x = Double.parseDouble(data[1]);
             double y = Double.parseDouble(data[2]);
             double z = Double.parseDouble(data[3]);
-            float yaw = Float.valueOf(data[4]);
-            float pitch = Float.valueOf(data[5]);
+            float yaw = Float.parseFloat(data[4]);
+            float pitch = Float.parseFloat(data[5]);
 
             return new Location(world, x, y, z, yaw, pitch);
         } catch (NumberFormatException ex) {
