3647874d5a4c7d97442a73464cecad29893487da
Aitooor <aitorarias19@gmail.com>
2023-03-23 04:36:36 +0100

    fix: added correctly PlayerListener on player join

--- src/main/java/online/starsmc/simplesetspawn/listener/PlayerListeners.java
+++ src/main/java/online/starsmc/simplesetspawn/listener/PlayerListeners.java
@@ -1,41 +1,64 @@
 package online.starsmc.simplesetspawn.listener;
 
 import online.starsmc.simplesetspawn.Main;
+import online.starsmc.simplesetspawn.utils.BukkitConfiguration;
+import online.starsmc.simplesetspawn.utils.ChatUtil;
 import online.starsmc.simplesetspawn.utils.location.LocationCodec;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import team.unnamed.inject.InjectAll;
 
+import javax.inject.Named;
 import java.util.Objects;
 
 @InjectAll
 public class PlayerListeners implements Listener {
 
     private Main plugin;
-    private FileConfiguration config;
+    @Named("spawns")
+    private BukkitConfiguration spawnsConfig;
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
+        String spawnLocation = spawnsConfig.get().getString("spawn_location");
+        String firstSpawnLocation = spawnsConfig.get().getString("first_spawn_location");
+
+        boolean joinSpawnBoolean = plugin.getConfig().getBoolean("onjoin_spawn");
+        boolean firstSpawnBoolean = plugin.getConfig().getBoolean("spawn_for_newbies");
+
+        if(spawnLocation == null && joinSpawnBoolean && player.hasPermission("simplesetspawn.admin")) {
+            ChatUtil.sendMsgPlayerPrefix(player, "&cRemember to setup Spawn location");
+        }
+
+        if(firstSpawnLocation == null && firstSpawnBoolean && player.hasPermission("simplesetspawn.admin")) {
+            ChatUtil.sendMsgPlayerPrefix(player, "&cRemember to setup First Spawn location");
+        }
+
         if(plugin.getConfig().getBoolean("on_first_join_spawn")) {
            if(!player.hasPlayedBefore()) {
-               player.teleport(Objects.requireNonNull(LocationCodec.deserialize(Objects.requireNonNull(config.getString("spawn_location")))));
+               if (spawnLocation != null) {
+                   player.teleport(Objects.requireNonNull(LocationCodec.deserialize(spawnLocation)));
+               }
            }
         }
 
-        if(plugin.getConfig().getBoolean("first_join_spawn")) {
+        if(firstSpawnBoolean) {
             if(!player.hasPlayedBefore()) {
-                player.teleport(Objects.requireNonNull(LocationCodec.deserialize(Objects.requireNonNull(config.getString("first_spawn_location")))));
+                if (firstSpawnLocation != null) {
+                    player.teleport(Objects.requireNonNull(LocationCodec.deserialize(firstSpawnLocation)));
+                }
             }
         }
 
         if(player.hasPlayedBefore()) {
-            if (plugin.getConfig().getBoolean("onjoin_spawn")) {
-                player.teleport(Objects.requireNonNull(LocationCodec.deserialize(Objects.requireNonNull(config.getString("spawn_location")))));
+            if (joinSpawnBoolean) {
+                if (spawnLocation != null) {
+                    player.teleport(Objects.requireNonNull(LocationCodec.deserialize(spawnLocation)));
+                }
             }
         }
     }
