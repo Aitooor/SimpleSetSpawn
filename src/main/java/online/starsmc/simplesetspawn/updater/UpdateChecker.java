84629787d63b4fc746b0cfc366973ef60780bdfd
Aitooor <aitorarias19@gmail.com>
2023-04-08 04:36:50 +0200

    fix: adding support

--- src/main/java/online/starsmc/simplesetspawn/updater/UpdateChecker.java
+++ src/main/java/online/starsmc/simplesetspawn/updater/UpdateChecker.java
@@ -20,6 +20,16 @@ public class UpdateChecker {
     }
 
     public void getVersion(final Consumer<String> consumer) {
+        if(plugin.getConfig().getBoolean("folia_support")) {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                plugin.getLogger().info("Unable to check for updates: " + exception.getMessage());
+            }
+            return;
+        }
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
             try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
                 if (scanner.hasNext()) {
