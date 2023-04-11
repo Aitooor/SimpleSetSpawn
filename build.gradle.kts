9120af7fd7807a338467a9f79363b108f38c633c
Aitooor <aitorarias19@gmail.com>
2023-04-11 18:28:48 +0200

    fix: added support to Folia end

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,19 +7,15 @@ group = "online.starsmc"
 var pluginVersion = "1.3.0"
 version = "no-shaded"
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(16))
-}
-
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://repo.unnamed.team/repository/unnamed-public/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("dev.folia:folia-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.2")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
